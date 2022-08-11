fd4bc63d64839c92b2ffb39223dc730f471b9b0c
alejo <alejovazquez276@gmail.com>
2022-08-11 18:41:25 -0300

    JSON estorage

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let productos = [
-    {id: 1, producto:"campera",color:"negro", precio: 0,stock:0},
-    {id:2, producto:"remera",color:"blanco",precio:0,stock:0},
-    {id:3,producto:"pantalon",color:"rojo",precio:0,stock:0},
-    {id:4,producto:"zapatilla",color:"azul",precio:0,stock:0}
+    {id: 1, producto:"campera", precio: 0,stock:0},
+    {id:2, producto:"remera",precio:0,stock:0},
+    {id:3,producto:"pantalon",precio:0,stock:0},
+    {id:4,producto:"zapatilla",precio:0,stock:0},
+    {id:4,producto:"medias",precio:0,stock:0},
+    {id:4,producto:"guantes",precio:0,stock:0}
 ]
 
 for (let i = 0; i < productos.length; i++) {
@@ -15,16 +17,40 @@ for (let i = 0; i < productos.length; i++) {
     productos[i].stock=entrada
 }
 
-console.log(productos);
+let contenedorProductos = document.getElementById("productos")
+console.dir(contenedorProductos)
 
-let nombres = productos.map((nombre) => nombre.producto)
 
-console.log(nombres);
 
-let resultado = productos.filter((producto) => producto.stock > 0)
+for(const producto of productos){
+    let tarjetaProducto=document.createElement('div')
+    tarjetaProducto.className="producto"
+    tarjetaProducto.innerHTML = `
+    <h3>${producto.producto}</h3>
+    <h4>$${producto.precio}</h4>
+    <p>Quedan ${producto.stock} u </p>
+    <div id = "contenedor"></div>
+    <button id = "boton">agregar al carrito</button>
+    
+    `
+    
+    contenedorProductos.append(tarjetaProducto)
+}
 
-console.log(resultado);
 
-let iva = 1.21
+let boton = document.getElementById('boton')
+let contenedor = document.getElementById('contenedor')
+let contador = 0
+boton.addEventListener('click',reemplazarNumero)
 
-let precioConIva = console.log(parseInt(prompt("Ingresar el precio del producto para sumarle el iva: "))*iva);
+function reemplazarNumero(){
+    contador++
+    contenedor.innerHTML = contador
+}
+console.log(JSON.stringify(productos));
+let productosJSON = JSON.stringify(productos)
+localStorage.setItem("productos", productosJSON)
+
+// let iva = 1.21
+
+// let precioConIva = console.log(parseInt(prompt("Ingresar el precio del producto para sumarle el iva: "))*iva);
